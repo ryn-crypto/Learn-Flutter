5ab5fb6f0950fc14745fe2211dc01d3788764a72
ryn-crypto <riyanfirst13@gmail.com>
2023-01-08 20:59:58 +0700

    Tab Bar Lanjutan

--- tab_bar/lib/main.dart
+++ tab_bar/lib/main.dart
@@ -9,31 +9,39 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    TabBar myTabBar = const TabBar(
+        indicator: BoxDecoration(
+            color: Colors.red,
+            border: Border(top: BorderSide(color: Colors.purple, width: 5))),
+        tabs: [
+          Tab(
+            icon: Icon(Icons.comment),
+            text: 'Comment',
+          ),
+          Tab(
+            child: Image(
+              image: AssetImage('assets/settings.png'),
+              color: Colors.white,
+            ),
+          ),
+          Tab(
+            icon: Icon(Icons.computer),
+          ),
+          Tab(
+            text: "News",
+          )
+        ]);
+
     return MaterialApp(
       home: DefaultTabController(
           length: 4,
           child: Scaffold(
             appBar: AppBar(
-              title: const Text('Contoh tab Bar'),
-              bottom: const TabBar(tabs: [
-                Tab(
-                  icon: Icon(Icons.comment),
-                  text: 'Comment',
-                ),
-                Tab(
-                  child: Image(
-                    image: AssetImage('assets/settings.png'),
-                    color: Colors.white,
-                  ),
-                ),
-                Tab(
-                  icon: Icon(Icons.computer),
-                ),
-                Tab(
-                  text: "News",
-                )
-              ]),
-            ),
+                title: const Text('Contoh Tab Bar'),
+                bottom: PreferredSize(
+                    preferredSize:
+                        Size.fromHeight(myTabBar.preferredSize.height),
+                    child: Container(color: Colors.amber, child: myTabBar))),
             body: const TabBarView(children: [
               Center(
                 child: Text("Tab 1"),
