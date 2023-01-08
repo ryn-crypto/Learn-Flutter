ca29dc8b95673092aa89a8dc1d2366419802c3a4
ryn-crypto <riyanfirst13@gmail.com>
2023-01-08 22:08:25 +0700

    menggunakan package qr_code

---
+++ qr_code/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          body: Center(
+        child: QrImage(
+          data: 'https://github.com/ryn-crypto',
+          version: 6,
+          backgroundColor: Colors.grey,
+          foregroundColor: Colors.black,
+          errorCorrectionLevel: QrErrorCorrectLevel.M,
+          padding: EdgeInsets.all(20),
+          size: 300,
+        ),
+      )),
+    );
+  }
+}
